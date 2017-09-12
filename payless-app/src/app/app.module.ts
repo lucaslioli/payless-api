be87c2632aac985b4722a409630636f2e5992df7
Luis Henrique Medeiros <lhmedeiros0@gmail.com>
2017-09-12 16:01:23 -0300

    [+] Template para mostrar produtos com list in cards adicionado

--- payless-app/src/app/app.module.ts
+++ payless-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { ProductsPageModule } from '../pages/products/products.module';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -18,6 +19,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    ProductsPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
