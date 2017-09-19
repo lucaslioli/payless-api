8fd215663f138cd678a5027b2615a5fbaf02cf39
lucaslioli <lucaslima.o@outlook.com>
2017-09-19 11:43:05 -0300

    Atualizada impressão dos dados do produto no extrator

--- misc/extract/extractor.php
+++ misc/extract/extractor.php
@@ -138,18 +138,31 @@
 	function print_nfce_data(Array $data){
 		foreach ($data as $key => $value) {
 			echo "<h3>".strtoupper($key)."</h3>";
+
+			if($key == "produtos")
+				echo "<table class='table'>
+						<tr>
+							<th>#</th>
+							<th>Código</th>
+							<th>Descricao</th>
+							<th>Valor</th>
+							<th>UN</th>
+						</tr>";
 			
 			foreach ($value as $k => $v) {
 				if(!is_array($v))
 					echo "<b>".$k.": </b>".$v."<br/>";
 				else{
-					echo "<h4>Produto ".$k."</h4>";
+					echo "<tr><td>".$k."</td>";
 					foreach ($v as $index => $inf) {
-						echo "<b> ".$index.": </b>".$inf."<br/>";
+						echo "<td>".$inf."</td>";
 					}
-					echo "<hr>";
+					echo "</tr>";
 				}
 			}
+
+			if($key == "produtos")
+				echo "</table>";
 
 			echo "<hr>";
 		}
